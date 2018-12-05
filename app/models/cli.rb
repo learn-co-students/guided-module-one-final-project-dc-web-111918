a5595dc67a3ad72c8a5dd4a6c575f628c80d6213
github username <github email address>
2018-12-05 14:00:14 -0500

    added area and eventtype methods to main CLI

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -1,92 +1,71 @@
 class Cli
-  attr_accessor :museum, :events, :area, :event_type, :whats_next
+  attr_accessor :area, :eventtype, :time, :museum, :events
 
-  def call
-    self.welcome
+  @@all = []
+
+  def self.all
+    @@all
   end
 
   def welcome
       puts "Welcome to the DC Event Locator."
-
-  def
-      puts "Please choose an area from the following option for event listings:"
-      puts "1. Smithsonian East"
-      puts "2. Smithsonian West"
-      puts "3. Capitol Hill"
-      puts "4. Chinatown"
-      puts "5. Foggy Bottom"
-      area = gets.chomp
-      if area == 1
-        puts "You've selected Mall North"
-      elsif area == 2
-        puts "You've selected Mall South"
-      elsif area == 3
-        puts "You've selected Capitol Hill"
-      elsif area == 4
-        puts "You've selected Chinatown"
-      elsif area == 5
-        puts "You've selected Foggy Bottom"
-      else
-        Please select a valid option from the
-        self.welcome
-      end
   end
 
-  def event_type
+  def area_prompt
+    puts "Please choose an area from the following option for event listings:"
+    puts "1. Mall North"
+    puts "2. Mall South"
+    puts "3. Capitol Hill"
+    puts "4. Chinatown"
+    puts "5. Foggy Bottom"
+      self.area = gets.strip
+  end
 
+  def area_selection
+    case self.area
+    when "1"
+      puts "You've selected MALL NORTH"
+    when "2"
+      puts "You've selected MALL SOUTH"
+    when "3"
+      puts "You've selected CAPITOL HILL"
+    when "4"
+      puts "You've selected CHINATOWN"
+    when "5"
+      puts "You've selected FOGGY BOTTOM"
+    else
+      puts "Invalid selection."
+      self.area_prompt
+    end
+  end
+
+
+  def eventtype_prompt
     puts "What kind of event are you interested in today?"
     puts "Please select from:"
     puts "1. Museum"
     puts "2. Lecture"
     puts "3. Concert"
     puts "4. Special Exhibition"
-
-    event_type = gets.chomp
-    if event_type == 1
-      self.how_much_time
-    elsif event_type == 2
-      self.how_much_time
-    elsif event_type == 3
-      self.how_much_time
-    elsif event_type == 4
-      self.how_much_time
-    else
-      "That didn't work"
-      self.event_type
-    end
+      self.eventtype = gets.strip
   end
 
-  def how_much_time
-    puts "How much time do you have?"
-    puts "Please select an amount of time." #### this might get gnarly with minutes. break it down?
-
-    puts "1. 30 Minutes"
-    puts "2. 1 hour"
-    puts "3. 2 hours"
-    puts "4. More than 2 hours"
-
-    area = gets.chomp
-    if area == 1
-      self.event_type
-    elsif area == 2
-      self.event_type
-    elsif area == 3
-      self.event_type
-    elsif area == 4
-      self.event_type
-    elsif area == 5
-      self.event_type
+  def eventtype_selection
+    case self.eventtype
+    when "1"
+      puts "You've selected MUSEUM"
+    when "2"
+      puts "You've selected LECTURE"
+    when "3"
+      puts "You've selected CONCERT"
+    when "4"
+      puts "You've selected SPECIAL EXHIBITION"
     else
-      "That didn't work"
-      self.welcome
-    end
-    else
-      puts "Sorry that doesn't work."
-      self.how_much_time
+      puts "Invalid selection."
+      self.eventtype_prompt
     end
   end
-
-
+  
   def list_events
 
     puts "Please select the event you're interested in to see more details."
@@ -122,3 +101,5 @@ class Cli
   end
 
 end
+
+def self.call
